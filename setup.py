fdaa5fa17127e127c5bdfad3a613119949a32eab
Raffaello Bonghi <raffaello@rnext.it>
2020-01-26 11:21:06 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['ros_jetson_stats'],
-    package_dir={'': 'src'})
+    package_dir={'': 'src'},
+    install_requires=['jetson-stats']
+    )
 
 setup(**setup_args)
