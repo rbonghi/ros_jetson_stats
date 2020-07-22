b80848e748ba90fa4fd30ae48baac2c037dcac71
Raffaello Bonghi <raffaello@rnext.it>
2020-07-22 19:47:38 +0100

    Move jtop wrapper to object

--- scripts/jetson_stats.py
+++ scripts/jetson_stats.py
@@ -31,6 +31,7 @@
 
 import rospy
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
+from ros_jetson_stats.srv import nvpmodel, nvpmodelResponse
 from datetime import timedelta
 import jtop
 # Import Diagnostic status converters
@@ -48,65 +49,93 @@ from ros_jetson_stats.utils import (
     emc_status)
 
 
-def wrapper(jetson):
+class ROSJtop:
+
+    def __init__(self, level_options):
+        self.level_options = level_options
+        interval = rospy.get_param("~interval", 0.5)
+        # Initialization jtop
+        self.jetson = jtop.jtop(interval=interval)
+        # Define Diagnostic array message
+        # http://docs.ros.org/api/diagnostic_msgs/html/msg/DiagnosticStatus.html
+        self.arr = DiagnosticArray()
+        # Initialization ros publisher
+        self.pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=1)
+        # Initialize nvpmodel service server
+        rospy.Service('nvpmodel', nvpmodel, self.nvpmodel_service)
+
+    def start(self):
+        self.jetson.start()
+        # Extract board information
+        board = self.jetson.board
+        # Define hardware name
+        self.hardware = board["info"]["machine"]
+        # Board status message
+        self.board_status = board_status(self.hardware, board, 'board')
+        # Set callback
+        rospy.loginfo("jtop wrapper started")
+        self.jetson.attach(self.jetson_callback)
+
+    def stop(self):
+        rospy.loginfo("Close jtop")
+        self.jetson.close()
+
+    def nvpmodel_service(self, req):
+        # New nvpmodel
+        nvpmodel = req.nvpmodel
+        rospy.loginfo(req)
+        return nvpmodelResponse(nvpmodel)
+
+    def jetson_callback(self, jetson):
+        # Add timestamp
+        self.arr.header.stamp = rospy.Time.now()
+        # Status board and board info
+        self.arr.status = [other_status(self.hardware, jetson, jtop.__version__)]
+        # Make diagnostic message for each cpu
+        self.arr.status += [cpu_status(self.hardware, name, jetson.cpu[name]) for name in jetson.cpu]
+        # Merge all other diagnostics
+        self.arr.status += [gpu_status(self.hardware, jetson.gpu)]
+        self.arr.status += [ram_status(self.hardware, jetson.ram, 'mem')]
+        self.arr.status += [swap_status(self.hardware, jetson.swap, 'mem')]
+        self.arr.status += [emc_status(self.hardware, jetson.emc, 'mem')]
+        # Temperature
+        self.arr.status += [temp_status(self.hardware, jetson.temperature, self.level_options)]
+        # Read power
+        total, power = jetson.power
+        if power:
+            self.arr.status += [power_status(self.hardware, total, power)]
+        # Fan controller
+        if jetson.fan is not None:
+            self.arr.status += [fan_status(self.hardware, jetson.fan, 'board')]
+        # Status board and board info
+        self.arr.status += [self.board_status]
+        # Add disk status
+        self.arr.status += [disk_status(self.hardware, jetson.disk, 'board')]
+        # Update status jtop
+        rospy.logdebug("jtop message %s" % rospy.get_time())
+        self.pub.publish(self.arr)
+
+
+def wrapper():
     # Load level options
     level_options = {
         rospy.get_param("~level/error", 60): DiagnosticStatus.ERROR,
         rospy.get_param("~level/warning", 40): DiagnosticStatus.WARN,
         rospy.get_param("~level/ok", 20): DiagnosticStatus.OK,
     }
+    # Initialization ros node
+    rospy.init_node('jtop_node')
+    # Initialization ROS jtop wrapper
+    jetson = ROSJtop(level_options)
     rospy.loginfo(level_options)
-    # Initialization ros publisher
-    pub = rospy.Publisher('/diagnostics', DiagnosticArray, queue_size=1)
-    # Extract board information
-    board = jetson.board
-    # Define hardware name
-    hardware = board["info"]["machine"]
-    # Define Diagnostic array message
-    # http://docs.ros.org/api/diagnostic_msgs/html/msg/DiagnosticStatus.html
-    arr = DiagnosticArray()
-    # Initialization jtop
-    while not rospy.is_shutdown() and jetson.ok():
-        # Add timestamp
-        arr.header.stamp = rospy.Time.now()
-        # Status board and board info
-        arr.status = [other_status(hardware, jetson, jtop.__version__)]
-        # Make diagnostic message for each cpu
-        arr.status += [cpu_status(hardware, name, jetson.cpu[name]) for name in jetson.cpu]
-        # Merge all other diagnostics
-        arr.status += [gpu_status(hardware, jetson.gpu)]
-        arr.status += [ram_status(hardware, jetson.ram, 'mem')]
-        arr.status += [swap_status(hardware, jetson.swap, 'mem')]
-        arr.status += [emc_status(hardware, jetson.emc, 'mem')]
-        # Temperature
-        arr.status += [temp_status(hardware, jetson.temperature, level_options)]
-        # Read power
-        total, power = jetson.power
-        if power:
-            arr.status += [power_status(hardware, total, power)]
-        # Fan controller
-        if jetson.fan is not None:
-            arr.status += [fan_status(hardware, jetson.fan, 'board')]
-        # Status board and board info
-        arr.status += [board_status(hardware, board, 'board')]
-        # Add disk status
-        arr.status += [disk_status(hardware, jetson.disk, 'board')]
-        # Update status jtop
-        rospy.logdebug("jtop message %s" % rospy.get_time())
-        pub.publish(arr)
+    # Start jetson
+    jetson.start()
+    # Spin ros
+    rospy.spin()
+    # Close jtop
+    jetson.stop()
 
 
 if __name__ == '__main__':
-    try:
-        # Initialization ros node
-        rospy.init_node('jtop_node')
-        # Load default interval speed
-        interval = rospy.get_param("~interval", 0.5)
-        # Run jtop reader
-        with jtop.jtop(interval=interval) as jetson:
-            wrapper(jetson)
-    except rospy.ROSInterruptException:
-        pass
-    except KeyboardInterrupt:
-        pass
+    wrapper()
 # EOF
